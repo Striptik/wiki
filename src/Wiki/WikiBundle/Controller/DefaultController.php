0a7c50a699e48dd16b21567d106dea6a19a6fdb3
Alexandre <a.simonin69@gmail.com>
2017-05-12 23:59:00 +0200

    [Clean Version] WikiWikiBundle

---
+++ src/Wiki/WikiBundle/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Wiki\WikiBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('@WikiWiki/Project/index.html.twig');
+    }
+}
---
+++ src/Wiki/WikiBundle/WikiWikiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wiki\WikiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WikiWikiBundle extends Bundle
+{
+}

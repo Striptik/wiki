6e5b893b81323bfc6fea432549c4738c11c4750e
Alexandre <a.simonin69@gmail.com>
2017-05-18 17:44:02 +0200

    Generate entity User + UserStatistic

---
+++ src/Wiki/WikiBundle/Entity/User.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace Wiki\WikiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="Wiki\WikiBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="status", type="integer")
+     */
+    private $status;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="pseudonyme", type="string", length=255, unique=true)
+     */
+    private $pseudonyme;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="role", type="integer")
+     */
+    private $role;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="lastconnected_at", type="datetime")
+     */
+    private $lastconnectedAt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     *
+     * @return User
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set pseudonyme
+     *
+     * @param string $pseudonyme
+     *
+     * @return User
+     */
+    public function setPseudonyme($pseudonyme)
+    {
+        $this->pseudonyme = $pseudonyme;
+
+        return $this;
+    }
+
+    /**
+     * Get pseudonyme
+     *
+     * @return string
+     */
+    public function getPseudonyme()
+    {
+        return $this->pseudonyme;
+    }
+
+    /**
+     * Set role
+     *
+     * @param integer $role
+     *
+     * @return User
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return int
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return User
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set lastconnectedAt
+     *
+     * @param \DateTime $lastconnectedAt
+     *
+     * @return User
+     */
+    public function setLastconnectedAt($lastconnectedAt)
+    {
+        $this->lastconnectedAt = $lastconnectedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get lastconnectedAt
+     *
+     * @return \DateTime
+     */
+    public function getLastconnectedAt()
+    {
+        return $this->lastconnectedAt;
+    }
+}
+
